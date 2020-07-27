7d7f9c1ae6fb007ec48b6ba7387c4e2f27e934a1
SpykeRel04D <roger.vidal.lloveras@gmail.com>
2020-07-27 15:09:14 +0200

    add comments on Blockchain class

--- blockchain.js
+++ blockchain.js
@@ -131,14 +131,30 @@ class Blockchain {
         this.miningReward = 100;
     }
 
+    /**
+     * @returns {Block}
+    */
     createGenesisBlock() {
         return new Block(Date.parse('2017-01-01'), [], '0');
     }
 
+    /**
+     * Returns the latest block on our chain. Useful when you want to create a
+     * new Block and you need the hash of the previous Block.
+     *
+     * @returns {Block[]}
+    */
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
+    /**
+     * Takes all the pending transactions, puts them in a Block and starts the
+     * mining process. It also adds a transaction to send the mining reward to
+     * the given address.
+     *
+     * @param {string} miningRewardAddress
+    */
     minePendingTransactions(miningRewardAddress) {
         const rewardTx = new Transaction(null, miningRewardAddress, this.miningReward);
         this.pendingTransactions.push(rewardTx);
@@ -152,6 +168,13 @@ class Blockchain {
         this.pendingTransactions = [];
     }
 
+    /**
+     * Add a new transaction to the list of pending transactions (to be added
+     * next time the mining process starts). This verifies that the given
+     * transaction is properly signed.
+     *
+     * @param {Transaction} transaction
+    */
     addTransaction(transaction) {
         if (!transaction.fromAddress || !transaction.toAddress) {
             throw new Error('Transaction must include from and to Address')
@@ -174,6 +197,12 @@ class Blockchain {
         console.log("Transaction added: ", transaction);
     }
 
+    /**
+     * Returns the balance of a given wallet address.
+     *
+     * @param {string} address
+     * @returns {number} The balance of the wallet
+    */
     getBalanceOfAddress(address) {
         let balance = 0;
 
@@ -193,6 +222,13 @@ class Blockchain {
         return balance;
     }
 
+    /**
+     * Returns a list of all transactions that happened
+     * to and from the given wallet address.
+     *
+     * @param  {string} address
+     * @return {Transaction[]}
+    */
     getAllTransactionsForWallet(address) {
         const txs = [];
     
@@ -208,13 +244,23 @@ class Blockchain {
         return txs;
     }
 
+    /**
+     * Loops over all the blocks in the chain and verify if they are properly
+     * linked together and nobody has tampered with the hashes. By checking
+     * the blocks it also verifies the (signed) transactions inside of them.
+     *
+     * @returns {boolean}
+    */
     isChainValid() {
-
+        // Check if the Genesis block hasn't been tampered with by comparing
+        // the output of createGenesisBlock with the first block on our chain
         const realGenesis = JSON.stringify(this.createGenesisBlock());
         if (realGenesis !== JSON.stringify(this.chain[0])) {
             return false;
         }
 
+        // Check the remaining blocks on the chain to see if there hashes and
+        // signatures are correct
         for (let i = 1; i < this.chain.length; i++) {
             const currentBlock = this.chain[i];
 
